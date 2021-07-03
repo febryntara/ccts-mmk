2d43703641ac58fffe86bf8c2b0f93ea917da3dd
febryntara <paglutgede@gmail.com>
2021-07-03 15:55:44 +0800

    component hero sudah full jadi

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Hero from "./components/Hero/Hero"
 
 function App() {
   return (
     <Router>
     <Navbar />
     <Switch>
-      <Route path='/' exact><h2>contant</h2></Route>
+      <Route path='/' exact><Hero /></Route>
     </Switch>
     </Router>
     
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Hero.css"
+
+function Hero() {
+    return (
+        <div className="m-hero">
+            <h3>Kuta Selatan</h3>
+        </div>
+    )
+}
+
+export default Hero
